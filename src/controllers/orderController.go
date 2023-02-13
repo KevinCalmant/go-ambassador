ef43216fe0aa6fcf15319b59c1c5e6bde5eefb16
kcalmant <kevin.calmant@ext.soprasteria.com>
2023-02-13 22:40:03 +0100

    feat(order + link): add preloading

--- src/controllers/orderController.go
+++ src/controllers/orderController.go
@@ -8,6 +8,10 @@ import (
 
 func Orders(c *fiber.Ctx) error {
 	var orders []models.Order
-	database.DB.Find(&orders)
+	database.DB.Preload("OrderItems").Find(&orders)
+	for i, order := range orders {
+		orders[i].Name = order.FullName()
+		orders[i].Total = order.GetTotal()
+	}
 	return c.JSON(orders)
 }
--- src/models/order.go
+++ src/models/order.go
@@ -6,15 +6,17 @@ type Order struct {
 	UserId          uint        `json:"userId"`
 	Code            string      `json:"code"`
 	AmbassadorEmail string      `json:"ambassadorEmail"`
-	FirstName       string      `json:"firstname"`
-	LastName        string      `json:"lastname"`
+	FirstName       string      `json:"-"`
+	LastName        string      `json:"-"`
+	Name            string      `json:"name" gorm:"-"`
 	Email           string      `json:"email"`
 	Address         string      `json:"address" gorm:"null"`
 	City            string      `json:"city" gorm:"null"`
 	Country         string      `json:"country" gorm:"null"`
 	Zip             string      `json:"zip" gorm:"null"`
-	Complete        bool        `json:"complete" gorm:"default:false"`
-	OrderItem       []OrderItem `json:"orderItem" gorm:"foreignKey:OrderId"`
+	Complete        bool        `json:"-" gorm:"default:false"`
+	Total           float64     `json:"total" gorm:"-""`
+	OrderItems      []OrderItem `json:"orderItems" gorm:"foreignKey:OrderId"`
 }
 
 type OrderItem struct {
@@ -25,4 +27,16 @@ type OrderItem struct {
 	Quantity          uint    `json:"quantity"`
 	AdminRevenue      float64 `json:"adminRevenue"`
 	AmbassadorRevenue float64 `json:"ambassadorRevenu"`
+}
+
+func (order *Order) FullName() string {
+	return order.FirstName + " " + order.LastName
+}
+
+func (order *Order) GetTotal() float64 {
+	var total float64 = 0
+	for _, orderItem := range order.OrderItems {
+		total += orderItem.Price * float64(orderItem.Quantity)
+	}
+	return total
 }
